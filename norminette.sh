f8959e782f6fbcf2a12eab36bbd1a4d2d8abf778
calculus <67384207+calculus-github@users.noreply.github.com>
2023-11-14 20:02:22 +0100

    Add files via upload

---
+++ norminette.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+while [ 1 = 1 ]
+do
+        echo "+===================================+"
+        echo "=Program coded and tested By Davidos="
+        echo "+===================================+"
+        uname -sn;pwd
+        echo "+===================================+"
+        norminette $PWD
+        sleep 5
+        clear
+done
